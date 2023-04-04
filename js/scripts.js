87d38c4856fa8d85c8dddeb97b690862b9e4be20
TiagoGermanio <97200746+TiagoGermanio@users.noreply.github.com>
2023-04-03 21:01:52 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// elements
+
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+const input = document.getElementsByClassName('h4');
+
+// functions
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+const getSymbol = () => {
+    const symbols = "(){}[]@#%&*<>/|";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = ""
+    const passwordLength = 10
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol,
+    ]
+    for(i=0; i < passwordLength; i = i + 4){
+        generators.forEach(() => {
+            const randomValue = 
+            generators[Math.floor(Math.random() * generators.length)]();
+            password += randomValue;
+        });
+    }
+    password = password.slice(0, passwordLength);
+
+    generatedPasswordElement.style.display = "block"
+    generatedPasswordElement.querySelector("h4").innerText = password;
+};
+// eventos 
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword(
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol,
+    );
+});
+console.log(generatePassword)
+function copyToClipboard (){
+    navigator.clipboard.writeText(input.value).then(()=>{
+        alert('Sua senha foi copiada para o clipboard')
+
+    })
+}
